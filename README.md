92759cc056316763230f6652d2ced2c0aa9a6e41
travis-tyler <travisatyler@gmail.com>
2021-01-16 11:37:07 -0600

    version_one now reads and appends to txt file

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # realtorAPI
 A program to send notifications when properties with certain specifications are listed.
+
+version_one uses a text file to keep track of existing listing IDs.
+
+version_two is unfinished and uses a DB to hold property info.
--- version_one/prop_push.py
+++ version_one/prop_push.py
@@ -17,7 +17,8 @@ querystring = {'city':'Austin',
                'sort':'newest',
                'prop_type': 'multi_family',
                'is_pending': False,
-               'is_contingent': False
+               'is_contingent': False,
+               'lot_sqft_min':'7405.2'
               }
 
 headers = {
@@ -36,6 +37,7 @@ def price_check(response):
     for x in response['listings']:
         listing = {}
         if int(x['price_raw']) <= 699000:
+            listing['id'] = x['listing_id']
             listing['address'] = x['address']
             listing['listdate'] = x['list_date'][:10]
             listing['price'] = x['short_price']
@@ -43,24 +45,53 @@ def price_check(response):
             new_listings.append(listing)
     return new_listings
 
-def notify(new_listings):
-    '''Send all listings via email'''
-    num = 0
-    body = ''
-    ts = time.time()
-    timestamp = datetime.datetime.fromtimestamp(ts).strftime('%m/%d %H:%M')
-    subject = f'MULTI-FAMILTY PROPERTIES FOR SALE - {timestamp}'
-    for i in new_listings:
-        num += 1
+def update(new_listings):
+    '''Updates txt file of listings IDs; sends email for every new listing'''
 
-        body += f"{num}. Address: {i['address']}\nList date: {i['listdate']}\nPrice: {i['price']}\nURL: {i['url']}\n\n---------\n\n"
-                    
+    # Server set up for email
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
     server.login(email_address, email_pass)
 
-    msg = f'Subject: {subject}\n\n{body}'
-    server.sendmail(email_address, recipient, msg)
+    # Opens listing file
+    with open('all_listings', 'r+') as f:
+        listings_all = f.read().splitlines()
+
+        # Checks if id already in file; appends if not
+        for i in new_listings:
+            if i['id'] not in listings_all:
+                f.write(f"{i['id']}\n")
+        
+                # Composes email and sends
+                subject = f"NEW MULTI-FAMILTY PROPERTY FOR SALE - {i['address']}"
+                body = f"Address: {i['address']}\nList date: {i['listdate']}\nPrice: {i['price']}\nURL: {i['url']}"
+                    
+                msg = f'Subject: {subject}\n\n{body}'
+                server.sendmail(email_address, recipient, msg)
+    f.close()
     server.quit()
 
-notify(price_check(query()))
+update(price_check(query()))
+
+
+# def notify(new_listings):
+#     '''Send ALL listings via email'''
+#     num = 0
+#     body = ''
+#     ts = time.time()
+#     timestamp = datetime.datetime.fromtimestamp(ts).strftime('%m/%d %H:%M')
+#     subject = f'MULTI-FAMILTY PROPERTIES FOR SALE - {timestamp}'
+#     for i in new_listings:
+#         num += 1
+
+#         body += f"{num}. Address: {i['address']}\nList date: {i['listdate']}\nPrice: {i['price']}\nURL: {i['url']}\n\n---------\n\n"
+                    
+#     server = smtplib.SMTP('smtp.gmail.com', 587)
+#     server.starttls()
+#     server.login(email_address, email_pass)
+
+#     msg = f'Subject: {subject}\n\n{body}'
+#     server.sendmail(email_address, recipient, msg)
+#     server.quit()
+
+# notify(price_check(query()))
